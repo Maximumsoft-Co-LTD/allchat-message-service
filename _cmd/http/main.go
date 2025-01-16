9827fe2b0e9aefe163cbcc3547f650999f4bfce2
Mack <mack@hpktechnology.com>
2025-01-16 11:50:28 +0700

    load env and create config

---
+++ _cmd/http/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"allchat-message-service/internal/adapter/config"
+	"log"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Green("Server starting...")
+	// _ = godotenv.Load()
+
+	// Load environment variables
+	config, err := config.New()
+	if err != nil {
+		log.Fatalf("Error loading environment variables", err)
+	}
+
+	// // database
+	// resource, err := mongoDB.New(config.DB)
+	// if err != nil {
+	// 	log.Fatalf("Connection database failure, Please check connection", err)
+	// }
+	// defer resource.Close()
+
+	// // Init cache service
+	// ctx := context.Background()
+	// cache, err := redis.New(ctx, config.Redis)
+	// if err != nil {
+	// 	log.Fatalf("Error initializing cache connection", err)
+	// }
+	// defer cache.Close()
+
+	// // Dependency injection
+	// //Room
+	// roomRepo := repository.NewRoomRepository(resource.DB)
+
+	// //Telegram
+	// telegramRepo := repository.NewTelegramRepository(resource.DB)
+	// telegramService := service.NewTelegramService(telegramRepo, roomRepo, cache)
+	// telegramHandler := http.NewTelegramHandler(telegramService)
+
+	// // Router
+	// router, err := http.NewRouter(
+	// 	config.HTTP,
+	// 	*telegramHandler,
+	// )
+	// if err != nil {
+	// 	log.Fatalf("Error initializing router", err)
+	// }
+
+	// // server
+	// fmt.Printf("Server listening on port %s\n", config.HTTP.Port)
+	// listenAddr := fmt.Sprintf(":%s", config.HTTP.Port)
+
+	// err = router.Serve(listenAddr)
+	// if err != nil {
+	// 	log.Fatalf("listen: %s\n", err)
+	// }
+}
